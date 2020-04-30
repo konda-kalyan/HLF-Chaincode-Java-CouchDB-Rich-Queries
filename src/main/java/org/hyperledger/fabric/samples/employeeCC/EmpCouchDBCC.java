c4806f0eba803e7b2417daae139f18a73a1c36da
Ubuntu <ubuntu@ip-172-31-8-85.us-east-2.compute.internal>
2020-04-30 09:13:31 +0000

    Completed: queryEmpBySalaryGreaterThanXAmount(), queryEmployees() functions

--- src/main/java/org/hyperledger/fabric/samples/employeeCC/EmpCouchDBCC.java
+++ src/main/java/org/hyperledger/fabric/samples/employeeCC/EmpCouchDBCC.java
@@ -28,7 +28,148 @@ public final class EmpCouchDBCC extends ChaincodeBase {
         }
     }
 
-    private String responseError(String errorMessage, String code) {
+     /**
+     * Chaincode 'init()' which gets called during Chaincode instantiation.
+     *
+     * @param ctx the transaction context
+     * @param key the key for the new Order
+     * @param Order JSON String
+     * @return the stored Order
+     */
+    
+    @Override
+    public Response init(ChaincodeStub stub) {
+        
+    	// Initialize ledger with some initial/dummy values
+    	List<String> args = stub.getParameters();
+    	
+    	if (args.size() != 5)	// Employee(long empID, String empName, String department, double salary, String location) 
+            return newErrorResponse(responseError("init: Incorrect number of arguments, expecting 5", ""));
+        
+        String key = args.get(0);
+        //Employee employee = new Employee(key, "Dummy", "Dummy", 0.0, "Dummy");
+        Employee employee = new Employee(key, args.get(1), args.get(2), Double.parseDouble(args.get(3)), args.get(4));
+        
+        try {
+            stub.putState(key, (new ObjectMapper()).writeValueAsBytes(employee));
+            return newSuccessResponse(responseSuccess("init: Dummy Employee added"));
+        } catch (Throwable e) {
+            return newErrorResponse(responseError(e.getMessage(), ""));
+        }
+    }
+    
+    @Override
+    public Response invoke(ChaincodeStub stub) {
+        String func = stub.getFunction();
+        List<String> params = stub.getParameters();
+        if (func.equals("addEmployee"))
+            return addEmployee(stub, params);
+        else if (func.equals("queryEmployee"))
+            return queryEmployee(stub, params);
+        else if (func.equals("queryEmpBySalaryGreaterThanXAmount"))
+            return queryEmpBySalaryGreaterThanXAmount(stub, params);
+        else if (func.equals("queryEmployees"))
+            return queryEmployees(stub, params);
+//        else if (func.equals("getWalletsWithTokenAmountGreaterThan"))
+//            return getWalletsWithTokenAmountGreaterThan(stub, params);
+        return newErrorResponse(responseError("Unsupported method", ""));
+    }
+    
+    private Response addEmployee(ChaincodeStub stub, List<String> args) {
+    	
+		// Employee(long empID, String empName, String department, double salary, String location)
+        if (args.size() != 5)
+            return newErrorResponse(responseError("addEmployee: Incorrect number of arguments, expecting 5", ""));
+        
+        String key = args.get(0);
+        Employee employee = new Employee(key, args.get(1), args.get(2), Double.parseDouble(args.get(3)), args.get(4));
+        
+        try {
+            if(checkString(stub.getStringState(key)))
+                return newErrorResponse(responseError("addEmployee: Employee already exists with employee ID: " + key, ""));
+            stub.putState(key, (new ObjectMapper()).writeValueAsBytes(employee));
+            return newSuccessResponse(responseSuccess("addEmployee: Employeed added"));
+        } catch (Throwable e) {
+            return newErrorResponse(responseError(e.getMessage(), ""));
+        }
+    }
+    
+	// Normal regular query (not a rich query)
+    private Response queryEmployee(ChaincodeStub stub, List<String> args) {
+    	
+		// args(0) - empID (nothing but key)
+        if (args.size() != 1)
+            return newErrorResponse(responseError("queryEmployee: Incorrect number of arguments, expecting 1", ""));
+            
+        try {
+            String empString = stub.getStringState(args.get(0));
+            if(!checkString(empString))
+                return newErrorResponse(responseError("queryEmployee: Employee doesn't exists", ""));
+            return newSuccessResponse((new ObjectMapper()).writeValueAsBytes(responseSuccessObject(empString)));
+        } catch(Throwable e){
+            return newErrorResponse(responseError(e.getMessage(), ""));
+        }
+    }
+    
+	// Parametarized rich query (prepare query string in this values)
+    private Response queryEmpBySalaryGreaterThanXAmount(ChaincodeStub stub, List<String> args) {
+    	
+		// args(0) - salary
+        if (args.size() != 1)
+            return newErrorResponse(responseError("queryEmpBySalaryGreaterThanXAmount: Incorrect number of arguments, expecting 1", ""));
+        
+        String salaryStr = args.get(0);
+
+        try {
+            double salary = Double.parseDouble(salaryStr);
+			System.out.println("Kalyan: queryEmpBySalaryGreaterThanXAmount: input salary amount: " + salary);
+            String queryStr = "{ \"selector\": { \"salary\": { \"$gt\": " + salary + " } }, \"use_index\":[\"_design/salaryIndexDoc\", \"salaryIndex\"] }";	// * always recommed to mention 'use_index'
+            String queryResult = query(stub, queryStr);
+			System.out.println("Kalyan: queryEmpBySalaryGreaterThanXAmount: queryResult: " + queryResult);
+            return newSuccessResponse((new ObjectMapper()).writeValueAsBytes(responseSuccessObject(queryResult)));
+        } catch(Throwable e){
+            return newErrorResponse(responseError(e.getMessage(), ""));
+        }
+    }
+	
+	// Ad hoc rich query (client sends query string and just use as it is)
+    private Response queryEmployees(ChaincodeStub stub, List<String> args) {
+    	
+		// args(0) - query string
+        if (args.size() != 1)
+            return newErrorResponse(responseError("queryEmpBySalaryGreaterThanXAmount: Incorrect number of arguments, expecting 1", ""));
+        
+        String queryString = args.get(0);
+
+        try {
+			// just use as it is we receive from client
+            String queryResult = query(stub, queryString);
+			System.out.println("Kalyan: queryEmpBySalaryGreaterThanXAmount: queryResult: " + queryResult);
+            return newSuccessResponse((new ObjectMapper()).writeValueAsBytes(responseSuccessObject(queryResult)));
+        } catch(Throwable e){
+            return newErrorResponse(responseError(e.getMessage(), ""));
+        }
+    }
+	
+	// helper function for rich queries
+    private String query(ChaincodeStub stub, String queryString) {
+        String result = "[";
+        QueryResultsIterator<KeyValue> rows = stub.getQueryResult(queryString);
+        while (rows.iterator().hasNext()) {
+            String v = rows.iterator().next().getStringValue();
+			System.out.println("Kalyan: query: individual record: " + v);
+            if(v != null && v.trim().length() > 0) {
+                result = result.concat(v);
+                if (rows.iterator().hasNext())
+                    result = result.concat(",");
+            }
+			System.out.println("Kalyan: query: concatinated result so far: " + result);
+        }
+        return result.concat("]");
+    }
+	
+	// helper functions for CC responses
+	private String responseError(String errorMessage, String code) {
         try {
             return (new ObjectMapper()).writeValueAsString(new ChaincodeResponse(errorMessage, code, false));
         } catch (Throwable e) {
@@ -52,118 +193,6 @@ public final class EmpCouchDBCC extends ChaincodeBase {
         if (str.trim().length() <= 0 || str == null)
             return false;
         return true;
-    }
-
-    /**
-     * Chaincode 'init()' which gets called during Chaincode instantiation.
-     *
-     * @param ctx the transaction context
-     * @param key the key for the new Order
-     * @param Order JSON String
-     * @return the stored Order
-     */
-    
-    @Override
-    public Response init(ChaincodeStub stub) {
-        
-    	// Initialize ledger with some initial/dummy values
-    	List<String> args = stub.getParameters();
-    	
-    	if (args.size() != 5)	// Employee(long empID, String empName, String department, double salary, String location) 
-            return newErrorResponse(responseError("init: Incorrect number of arguments, expecting 5", ""));
-        
-        String key = args.get(0);
-        //Employee employee = new Employee(key, "Dummy", "Dummy", 0.0, "Dummy");
-        Employee employee = new Employee(key, args.get(1), args.get(2), Double.parseDouble(args.get(0)), args.get(4));
-        
-        try {
-            stub.putState(key, (new ObjectMapper()).writeValueAsBytes(employee));
-            return newSuccessResponse(responseSuccess("init: Dummy Employee added"));
-        } catch (Throwable e) {
-            return newErrorResponse(responseError(e.getMessage(), ""));
-        }
-    }
-    
-    @Override
-    public Response invoke(ChaincodeStub stub) {
-        String func = stub.getFunction();
-        List<String> params = stub.getParameters();
-        if (func.equals("addEmployee"))
-            return addEmployee(stub, params);
-        else if (func.equals("queryEmployee"))
-            return queryEmployee(stub, params);
-        else if (func.equals("queryEmpBySalaryGreaterThanXAmount"))
-            return queryEmpBySalaryGreaterThanXAmount(stub, params);
-//        else if (func.equals("getWalletsWithTokenAmountGreaterThan"))
-//            return getWalletsWithTokenAmountGreaterThan(stub, params);
-        return newErrorResponse(responseError("Unsupported method", ""));
-    }
-    
-    private Response addEmployee(ChaincodeStub stub, List<String> args) {
-    	
-        if (args.size() != 5)	// Employee(long empID, String empName, String department, double salary, String location) 
-            return newErrorResponse(responseError("addEmployee: Incorrect number of arguments, expecting 5", ""));
-        
-        String key = args.get(0);
-        Employee employee = new Employee(key, args.get(1), args.get(2), Double.parseDouble(args.get(0)), args.get(4));
-        
-        try {
-            if(checkString(stub.getStringState(key)))
-                return newErrorResponse(responseError("addEmployee: Employee already exists with employee ID: " + key, ""));
-            stub.putState(key, (new ObjectMapper()).writeValueAsBytes(employee));
-            return newSuccessResponse(responseSuccess("addEmployee: Employeed added"));
-        } catch (Throwable e) {
-            return newErrorResponse(responseError(e.getMessage(), ""));
-        }
-    }
-    
-    private Response queryEmployee(ChaincodeStub stub, List<String> args) {
-    	
-        if (args.size() != 1)
-            return newErrorResponse(responseError("queryEmployee: Incorrect number of arguments, expecting 1", ""));
-        
-        String empID = args.get(0);
-        
-        try {
-            String empString = stub.getStringState(empID);
-            if(!checkString(empString))
-                return newErrorResponse(responseError("queryEmployee: Employee doesn't exists", ""));
-            return newSuccessResponse((new ObjectMapper()).writeValueAsBytes(responseSuccessObject(empString)));
-        } catch(Throwable e){
-            return newErrorResponse(responseError(e.getMessage(), ""));
-        }
-    }
-    
-    private Response queryEmpBySalaryGreaterThanXAmount(ChaincodeStub stub, List<String> args) {
-    	
-        if (args.size() != 1)
-            return newErrorResponse(responseError("queryEmpBySalaryGreaterThanXAmount: Incorrect number of arguments, expecting 1", ""));
-        
-        String salaryStr = args.get(0);
-
-        try {
-            double salary = Double.parseDouble(salaryStr);
-            String queryStr = "{ \"selector\": { \"salary\": { \"$gt\": " + salary + " } } }";
-            String queryResult = query(stub, queryStr);
-            return newSuccessResponse((new ObjectMapper()).writeValueAsBytes(responseSuccessObject(queryResult)));
-        } catch(Throwable e){
-            return newErrorResponse(responseError(e.getMessage(), ""));
-        }
-    }
-    
-    
-    private String query(ChaincodeStub stub, String queryString) {
-        String result = "[";
-        QueryResultsIterator<KeyValue> rows = stub.getQueryResult(queryString);
-        while (rows.iterator().hasNext()) {
-            String v = rows.iterator().next().getStringValue();
-            if(v != null && v.trim().length() > 0) {
-                result = result.concat(v);
-                if (rows.iterator().hasNext())
-                    result = result.concat(",");
-            }
-        }
-        return result.concat("]");
     }
 
     public static void main(String[] args) {
